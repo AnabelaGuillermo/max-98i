0be9aea9bd34bbe6b893d7b8699c1741671c5925
Santiago Altamiranda <santiagoaltamiranda1997@gmail.com>
2024-06-02 00:55:56 -0300

    Solucionando problemas, de inicio de sesion, alerts de inicio de sesion.

--- js/utils.js
+++ js/utils.js
@@ -1,20 +1,26 @@
 // utils.js
-export function obtenerCategoriasDeLS() {
-    return JSON.parse(localStorage.getItem('categorias')) || [];
-}
 
-export function guardarCategoriasEnLS(categorias) {
-    localStorage.setItem('categorias', JSON.stringify(categorias));
-}
-
+// Importa la función para obtener todas las películas del almacenamiento local
 export function obtenerPeliculasDeLS() {
     return JSON.parse(localStorage.getItem('peliculas')) || [];
 }
 
+// Importa la función para guardar las películas en el almacenamiento local
 export function guardarPeliculasEnLS(peliculas) {
     localStorage.setItem('peliculas', JSON.stringify(peliculas));
 }
 
+// Define la función para obtener las películas destacadas
+export function obtenerPeliculasDestacadas() {
+    // Obtiene todas las películas del almacenamiento local
+    const peliculas = obtenerPeliculasDeLS();
+    // Filtra las películas destacadas
+    const peliculasDestacadas = peliculas.filter(pelicula => pelicula.destacado);
+    // Retorna las películas destacadas
+    return peliculasDestacadas;
+}
+
+// Exporta las funciones para obtener y guardar series en el almacenamiento local
 export function obtenerSeriesDeLS() {
     return JSON.parse(localStorage.getItem('series')) || [];
 }
@@ -23,10 +29,11 @@ export function guardarSeriesEnLS(series) {
     localStorage.setItem('series', JSON.stringify(series));
 }
 
-export const obtenerUsuarioDeLs =()=>{
-    return JSON.parse(localStorage.getItem('usuarios')) || [];
+// Exporta las funciones para obtener y guardar categorías en el almacenamiento local
+export function obtenerCategoriasDeLS() {
+    return JSON.parse(localStorage.getItem('categorias')) || [];
 }
 
-export const obtenerComentarioDeLs =()=>{
-    return JSON.parse(localStorage.getItem('comentarios')) || [];
+export function guardarCategoriasEnLS(categorias) {
+    localStorage.setItem('categorias', JSON.stringify(categorias));
 }
